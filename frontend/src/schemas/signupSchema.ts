df209013edee771dd2a78394ca1ef663aa06fe3f
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-13 00:54:25 +0800

    feat: create a schema for sign-up

--- frontend/src/schemas/signupSchema.ts
+++ frontend/src/schemas/signupSchema.ts
@@ -1,8 +1,15 @@
 import {z} from "zod"
 
-export const signupSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8, "Password must be at least 6 characters long."),
+export const signupSchema = z
+.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z.
+    string()
+    .min(8, {message: "Password must be at least 8 characters long."})
+    .regex(/[a-z]/, {message: "Password must contain at least one lowercase letter."})
+    .regex(/[A-Z]/, {message: "Password must contain at least one uppercase letter."})
+    .regex(/[0-9]/, {message: "Password must contain at least one number."})
+    .regex(/[^a-zA-Z0-9]/, {message: "Password must contain at least one special character."}),
   passwordConfirm: z.string()
 }).refine((data) => data.password === data.passwordConfirm, {
   message: "Passwords don't match",
