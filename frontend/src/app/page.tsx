3f74f842d63a4352ad982d1d59711a7b05799f2d
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-14 20:40:17 +0800

    feat(ui): landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,12 @@
+import Navbar from "@/components/navbar";
 import LandingPage from "@/components/pages/landing-page";
 
 export default function Home() {
   return (
-    <LandingPage/>
+    <main>
+      <Navbar/>
+      <LandingPage/>
+    </main>
+    
   );
 }
