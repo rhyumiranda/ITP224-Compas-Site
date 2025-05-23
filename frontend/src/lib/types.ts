050e76267d19bffed19a163eeb362813bb550334
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-05-23 10:36:01 +0800

    chore: create new static types

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -18,3 +18,29 @@ export interface SignUpFormErrorProps {
 export interface ForgotPasswordFormProps {
   email: string
 }
+
+export interface CreateNewPasswordFormProps {
+  password: string
+  passwordConfirm: string
+}
+
+export interface CreateNewPasswordFormErrorProps {
+  emailError: string | null
+  passwordError: string | null
+  passwordConfirmError: string | null
+}
+
+export interface VerifyOtpFormProps {
+  otp: string
+}
+
+export interface TravelLogProps {
+    id: number,
+    user_id: string,
+    city: string,
+    country: string,
+    date: string,
+    activities: string[],
+    information: string[],
+    created_at: string,
+}
